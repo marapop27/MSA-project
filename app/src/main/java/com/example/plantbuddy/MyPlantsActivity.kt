25fc432a5334fd53ca4718d455945e2621792d64
Andrada Povian <andrada.povian98@gmail.com>
2020-11-02 18:01:34 +0200

    added list view using recycler adapter

--- app/src/main/java/com/example/plantbuddy/MyPlantsActivity.kt
+++ app/src/main/java/com/example/plantbuddy/MyPlantsActivity.kt
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.plantbuddy.recyclerView.BlogRecyclerAdapter
+import com.example.plantbuddy.recyclerView.DataSource
+import com.example.plantbuddy.recyclerView.TopSpacingItemDecoration
+import kotlinx.android.synthetic.main.activity_my_plants.*
 import kotlinx.android.synthetic.main.toolbar_with_back_button.*
 
 class MyPlantsActivity : AppCompatActivity() {
+
+    private lateinit var blogAdapter: BlogRecyclerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_plants)
@@ -14,6 +22,25 @@ class MyPlantsActivity : AppCompatActivity() {
         var backButton = findViewById<ImageView>(R.id.btn_toolbar_back);
         backButton.setOnClickListener {
             onBackPressed();
+        }
+
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun addDataSet(){
+        val data = DataSource.createDataSet()
+        blogAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView(){
+
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MyPlantsActivity)
+            val topSpacingDecoration = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecoration)
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
         }
     }
 }
---
+++ app/src/main/java/com/example/plantbuddy/recyclerView/BlogPost.kt
@@ -0,0 +1,10 @@
+package com.example.plantbuddy.recyclerView
+
+
+data class BlogPost(
+    var title: String,
+    var body: String,
+    var image: String,
+    var username: String
+) {
+}
